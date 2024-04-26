b8ddf1f97bf1349d9cf9827a4a03066c75d6b4ab
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-26 22:54:50 +0200

    Add emacs-nox to dependencies

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
             devShells.default = pkgs.mkShell {
               buildInputs = with pkgs; [
                 nodejs
+                emacs-nox
               ];
             };
           });
