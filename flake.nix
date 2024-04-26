7b702892bca6b58de3306ff82ca864fc8fe85d71
Jeff Kreeftmeijer <jeff@kreeft.me>
2024-04-26 22:49:37 +0200

    Move node dependency to flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... } :
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+          {
+            devShells.default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                nodejs
+              ];
+            };
+          });
+}
